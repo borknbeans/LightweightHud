f4630ded86ad75fe7bdccc354f32d2771e1fa530
Ethan Bork <ebork11@gmail.com>
2024-06-23 09:29:54 -0700

    Initial Commit

---
+++ src/client/java/borknbeans/lightweighthud/LightweightHUDClient.java
@@ -0,0 +1,10 @@
+package borknbeans.lightweighthud;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LightweightHUDClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
